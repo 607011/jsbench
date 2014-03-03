46a76ed12566921e61a1997a03427b5df4e55471
Oliver Lau <ola@ct.de>
2014-03-03 16:24:06 +0100

    `Clock` -> `Stopwatch`.

--- js/bench.js
+++ js/bench.js
@@ -14,25 +14,16 @@
   ********************************************************************/
 
   var REPS = 100000000;
+  var NUM_RUNS = 5;
+  var nRun = 0;
   var out;
   var results = {};
 
-  var Clock = (function () {
+  var Stopwatch = (function () {
     var t0 = Date.now();
-    var t00 = Date.now();
     return {
-      ms: function () {
-        return Date.now() - t00;
-      },
-      secs: function () {
-        var now = Date.now();
-        var t = now - t0;
-        t0 = now;
-        return 1e-3 * t;
-      },
-      reset: function () {
-        t00 = t0 = Date.now();
-      }
+      elapsed: function () { return Date.now() - t0; },
+      start: function () { t0 = Date.now(); }
     };
   })();
 
@@ -51,7 +42,7 @@
     if (delay === undefined)
       delay = 100;
     setTimeout(function () {
-      Clock.reset();
+      Stopwatch.start();
       f.call();
       promise.resolve();
     }, delay /* time to repaint DOM */);
@@ -65,7 +56,7 @@
       for (i = 0; i < N; ++i) {
         sum += i;
       }
-      print_result('<code>for (...)</code>', N, Clock.ms());
+      print_result('<code>for (...)</code>', N, Stopwatch.elapsed());
     });
   }
 
@@ -76,7 +67,7 @@
       while (i--) {
         sum += i;
       }
-      print_result('<code>while (i--)</code>', N, Clock.ms());
+      print_result('<code>while (i--)</code>', N, Stopwatch.elapsed());
     });
   }
 
@@ -90,7 +81,7 @@
         else if (i > 50000000)
           continue;
       }
-      print_result('<code>if (i < 50000000) ...</code>', N, Clock.ms());
+      print_result('<code>if (i < 50000000) ...</code>', N, Stopwatch.elapsed());
     });
   }
 
@@ -104,7 +95,7 @@
         else if (i > C)
           continue;
       }
-      print_result('<code>if (i < C) ...</code>', N, Clock.ms());
+      print_result('<code>if (i < C) ...</code>', N, Stopwatch.elapsed());
     });
   }
 
@@ -115,7 +106,7 @@
       for (i = 0; i < N; ++i) {
         v.push(Math.random());
       }
-      print_result('generating floats with <code>Math.random()</code>', N, Clock.ms());
+      print_result('generating floats with <code>Math.random()</code>', N, Stopwatch.elapsed());
       v = undefined;
     });
   }
@@ -127,7 +118,7 @@
       for (i = 0; i < N; ++i) {
         v.push(rng.genrand_int32() / RNG.MAX_VALUE);
       }
-      print_result('generating floats with <code>MersenneTwister()</code>', N, Clock.ms());
+      print_result('generating floats with <code>MersenneTwister()</code>', N, Stopwatch.elapsed());
       v = undefined;
     });
   }
@@ -139,7 +130,7 @@
       for (i = 0; i < N; ++i) {
         v.push(rng.next() / RNG.MAX_VALUE);
       }
-      print_result('generating floats with LCG', N, Clock.ms());
+      print_result('generating floats with LCG', N, Stopwatch.elapsed());
       v = undefined;
     });
   }
@@ -151,7 +142,7 @@
       for (i = 0; i < N; ++i) {
         v.push(Math.floor(Math.random() * RNG.MAX_VALUE));
       }
-      print_result('generating integers with <code>Math.random()</code>', N, Clock.ms());
+      print_result('generating integers with <code>Math.random()</code>', N, Stopwatch.elapsed());
       v = undefined;
     });
   }
@@ -163,7 +154,7 @@
       for (i = 0; i < N; ++i) {
         v.push(rng.genrand_int32());
       }
-      print_result('generating integers with <code>MersenneTwister()</code>', N, Clock.ms());
+      print_result('generating integers with <code>MersenneTwister()</code>', N, Stopwatch.elapsed());
       v = undefined;
     });
   }
@@ -175,7 +166,7 @@
       for (i = 0; i < N; ++i) {
         v.push(rng.next());
       }
-      print_result('generating integers with LCG', N, Clock.ms());
+      print_result('generating integers with LCG', N, Stopwatch.elapsed());
       v = undefined;
     });
   }
@@ -187,7 +178,7 @@
       for (i = 0; i < N; ++i) {
         v = new Array(2);
       }
-      print_result('<code>new Array(2)</code>', N, Clock.ms());
+      print_result('<code>new Array(2)</code>', N, Stopwatch.elapsed());
       v = undefined;
     });
   }
@@ -199,7 +190,7 @@
       for (i = 0; i < N; ++i) {
         v = [0, 0];
       }
-      print_result('<code>[0, 0]</code>', N, Clock.ms());
+      print_result('<code>[0, 0]</code>', N, Stopwatch.elapsed());
       v = undefined;
     });
   }
@@ -211,7 +202,7 @@
       for (i = 0; i < N; ++i) {
         v = { x: 0, y: 0 };
       }
-      print_result('<code>{x:0, y:0}</code>', N, Clock.ms());
+      print_result('<code>{x:0, y:0}</code>', N, Stopwatch.elapsed());
       v = undefined;
     });
   }
@@ -223,7 +214,7 @@
       for (i = 0; i < N; ++i) {
         v = new Float32Array(2);
       }
-      print_result('<code>new Float32Array(2)</code>', N, Clock.ms());
+      print_result('<code>new Float32Array(2)</code>', N, Stopwatch.elapsed());
       v = undefined;
     });
   }
@@ -235,7 +226,7 @@
       for (i = 0; i < N; ++i) {
         v = new Float64Array(2);
       }
-      print_result('<code>new Float64Array(2)</code>', N, Clock.ms());
+      print_result('<code>new Float64Array(2)</code>', N, Stopwatch.elapsed());
       v = undefined;
     });
   }
@@ -249,7 +240,7 @@
       for (i = 1; i < N; ++i) {
         v[i] = v[i - 1] + i;
       }
-      print_result('loop over <code>[]</code>', N, Clock.ms());
+      print_result('loop over <code>[]</code>', N, Stopwatch.elapsed());
       v = undefined;
     });
   }
@@ -263,7 +254,7 @@
       for (i = 1; i < N; ++i) {
         v[i] = v[i - 1] + i;
       }
-      print_result('loop over <code>Array()</code>', N, Clock.ms());
+      print_result('loop over <code>Array()</code>', N, Stopwatch.elapsed());
       v = undefined;
     });
   }
@@ -275,7 +266,7 @@
       for (i = 1; i < N; ++i) {
         v[i] = v[i - 1] + i;
       }
-      print_result('loop over <code>Float32Array()</code>', N, Clock.ms());
+      print_result('loop over <code>Float32Array()</code>', N, Stopwatch.elapsed());
       v = undefined;
     });
   }
@@ -287,7 +278,7 @@
       for (i = 1; i < N; ++i) {
         v[i] = v[i - 1] + i;
       }
-      print_result('loop over <code>Float64Array()</code>', N, Clock.ms());
+      print_result('loop over <code>Float64Array()</code>', N, Stopwatch.elapsed());
       v = undefined;
     });
   }
@@ -299,7 +290,7 @@
       for (i = 1; i < N; ++i) {
         v[i] = v[i - 1] + i;
       }
-      print_result('loop over <code>Int32Array()</code>', N, Clock.ms());
+      print_result('loop over <code>Int32Array()</code>', N, Stopwatch.elapsed());
       v = undefined;
     });
   }
@@ -313,7 +304,7 @@
         x = i;
         y = sum;
       }
-      print_result('write to <code>x</code>, <code>y</code>', N, Clock.ms());
+      print_result('write to <code>x</code>, <code>y</code>', N, Stopwatch.elapsed());
     });
   }
 
@@ -326,7 +317,7 @@
         obj[0] = i;
         obj[1] = sum;
       }
-      print_result('write to <code>[0, 0]</code>', N, Clock.ms());
+      print_result('write to <code>[0, 0]</code>', N, Stopwatch.elapsed());
     });
   }
 
@@ -339,7 +330,7 @@
         obj.x = i;
         obj.y = sum;
       }
-      print_result('write to <code>{x:0, y:0}</code>', N, Clock.ms());
+      print_result('write to <code>{x:0, y:0}</code>', N, Stopwatch.elapsed());
     });
   }
 
@@ -352,7 +343,7 @@
         obj[0] = i;
         obj[1] = sum;
       }
-      print_result('write to <code>Float32Array(2)</code>', N, Clock.ms());
+      print_result('write to <code>Float32Array(2)</code>', N, Stopwatch.elapsed());
     });
   }
 
@@ -363,7 +354,7 @@
       for (i = 0; i < N; ++i) {
         sum += x + y;
       }
-      print_result('read from <code>x</code>, <code>y</code>', N, Clock.ms());
+      print_result('read from <code>x</code>, <code>y</code>', N, Stopwatch.elapsed());
     });
   }
 
@@ -374,7 +365,7 @@
       for (i = 0; i < N; ++i) {
         sum += obj[0] + obj[1];
       }
-      print_result('read from <code>[0, 0]</code>', N, Clock.ms());
+      print_result('read from <code>[0, 0]</code>', N, Stopwatch.elapsed());
     });
   }
 
@@ -385,7 +376,7 @@
       for (i = 0; i < N; ++i) {
         sum += obj.x + obj.y;
       }
-      print_result('read from <code>{x:0, y:0}</code>', N, Clock.ms());
+      print_result('read from <code>{x:0, y:0}</code>', N, Stopwatch.elapsed());
     });
   }
 
@@ -398,15 +389,10 @@
       for (i = 0; i < N; ++i) {
         sum += obj[0] + obj[1];
       }
-      print_result('read from <code>Float32Array(2)</code>', N, Clock.ms());
+      print_result('read from <code>Float32Array(2)</code>', N, Stopwatch.elapsed());
     });
   }
 
-
-
-
-  var NUM_RUNS = 5;
-  var nRun = 0;
 
   function ready() {
     if (++nRun < NUM_RUNS) {
